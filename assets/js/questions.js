d1e57267db272dcb6c41934fa2952cf762fb0c38
Sam Callaway <calls002@gmail.com>
2023-01-29 14:04:06 +0000

    Wrote a set of 10 questions for the quiz

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -5,60 +5,58 @@ questions = [
         correctAnswer: "%"
     },
     {
-        question:"Which of these is the modulo operator in JavaScript?",
-        answers: ["+","=","%","$"],
-        correctAnswer: "%"
+        question:"What does the acronym OOP stand for?",
+        answers: ["Object Oriented Paragraphs","Object Oriented Protocols","Object Oriented Programming","Object Oriented Procedures"],
+        correctAnswer: "Object Oriented Programming"
     },
     {
-        question:"Which of these is the modulo operator in JavaScript?",
-        answers: ["+","=","%","$"],
-        correctAnswer: "%"
+        question:"What is a variable?",
+        answers: ["A string of text","A named storage location for data","A collection of data","A mathematical equation"],
+        correctAnswer: "A named storage location for data"
     },
     {
-        question:"Which of these is the modulo operator in JavaScript?",
-        answers: ["+","=","%","$"],
-        correctAnswer: "%"
+        question:"What is the purpose of using a loop?",
+        answers: ["To run a block of code once","To repeat a block of code","To print a message on the screen","To pause the program for a certain amount of time"],
+        correctAnswer: "To repeat a block of code"
     },
     {
-        question:"Which of these is the modulo operator in JavaScript?",
-        answers: ["+","=","%","$"],
-        correctAnswer: "%"
+        question:"What is a function?",
+        answers: ["A way to debug a program","A data structure for storing variables","A loop that executes code multiple times","A named block of code that performs a specific task"],
+        correctAnswer: "A named block of code that performs a specific task."
     },
     {
-        question:"Which of these is the modulo operator in JavaScript?",
-        answers: ["+","=","%","$"],
-        correctAnswer: "%"
+        question:"What does the acronym API stand for?",
+        answers: ["Accessable Program Interface","Application Protocol Interface","Application Program Interaction","Application Programming Interface"],
+        correctAnswer: "Application Programming Interface"
     },
     {
-        question:"Which of these is the modulo operator in JavaScript?",
-        answers: ["+","=","%","$"],
-        correctAnswer: "%"
+        question:"How do you add a comment in Javascript?",
+        answers: ["// This is a comment","# This is a comment","<!-- This is a comment -->","<! This is a comment >"],
+        correctAnswer: "// This is a comment"
     },
     {
-        question:"Which of these is the modulo operator in JavaScript?",
-        answers: ["+","=","%","$"],
-        correctAnswer: "%"
+        question:"What does HTML stand for?",
+        answers: ["Hyper Text Markup Language","High Tech Markup Language","Hyper Text Main Link","High Tech Main Link"],
+        correctAnswer: "Hyper Text Markup Language"
     },
     {
-        question:"Which of these is the modulo operator in JavaScript?",
-        answers: ["+","=","%","$"],
-        correctAnswer: "%"
+        question:"What does CSS stand for?",
+        answers: ["Colorful Style Sheets","Cascading Style Sheets","Creative Standard Sheets","Creative Style Sheets"],
+        correctAnswer: "Cascading Style Sheets"
     },
     {
-        question:"Which of these is the modulo operator in JavaScript?",
-        answers: ["+","=","%","$"],
-        correctAnswer: "%"
+        question:"Which of the following is not a CSS selector?",
+        answers: ["ID","Class","Element","Number"],
+        correctAnswer: "Number"
     },
     {
-        question:"Which of these is the modulo operator in JavaScript?",
-        answers: ["+","=","%","$"],
-        correctAnswer: "%"
+        question:"Why should we use semantic HTML?",
+        answers: ["To make code easier to read for developers","For search engine optimisation","To help screen readers read the webpage","All of the Above"],
+        correctAnswer: "All of the Above"
     },
     {
-        question:"Which of these is the modulo operator in JavaScript?",
-        answers: ["+","=","%","$"],
-        correctAnswer: "%"
-    }
-
-    
+        question:"Which of these increase the value of i by 1?",
+        answers: ["i == i + 1","i--","i++","i+1"],
+        correctAnswer: "i++"
+    }    
 ]
